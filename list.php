01c466cf21c46341ef4a7d583f137a90eb79fb41
Lynok <lazarenko.alona@yandex.ru>
2014-05-08 10:39:17 +0300

    Create list.php

---
+++ list.php
@@ -0,0 +1,32 @@
+<?php
+
+mysql_connect("localhost","-u root","");
+echo "<table border=2>\n";
+echo "<tr><td>surname</td>
+            <td>name</td>
+            <td>fathersname</td>
+            <td>age</td>
+            <td>birthday</td>
+            <td>sex</td>
+            <td>edit</td>
+            <td>delete</td>
+            </tr>\n";
+mysql_query("use information");
+$r = mysql_query("select * from information");
+$n = mysql_num_rows($r);
+for ($i=0;$i<$n;$i++)
+{
+	$f = mysql_fetch_array($r);
+	//var_dump($f);
+    echo "<tr><td>".$f['surname']."</td>
+            <td>".$f['name']."</td>
+            <td>".$f['fathersname']."</td>
+            <td>".$f['age']."</td>
+            <td>".$f['birthday']."</td>
+            <td>".$f['sex']."</td>
+            <td><a href='edit.php?id=".$f['id']."'>edit</a></td>
+            <td><a href='delete.php?id=".$f['id']."'>delete</a></td>
+            </tr>\n";
+
+}
+?>
