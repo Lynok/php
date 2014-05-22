93acf048e58e919b0426d6b58846ca0dfb7551eb
Lynok <lazarenko.alona@yandex.ru>
2014-05-22 09:35:28 +0300

    Update list.php

--- list.php
+++ list.php
@@ -5,9 +5,10 @@ echo "<table border=2>\n";
 echo "<tr><td>surname</td>
             <td>name</td>
             <td>fathersname</td>
-            <td>age</td>
-            <td>birthday</td>
-            <td>sex</td>
+            <td>city</td>
+            <td>adress</td>
+            <td>product</td>
+            <td>quantity</td>
             <td>edit</td>
             <td>delete</td>
             </tr>\n";
@@ -21,9 +22,10 @@ for ($i=0;$i<$n;$i++)
     echo "<tr><td>".$f['surname']."</td>
             <td>".$f['name']."</td>
             <td>".$f['fathersname']."</td>
-            <td>".$f['age']."</td>
-            <td>".$f['birthday']."</td>
-            <td>".$f['sex']."</td>
+            <td>".$f['city']."</td>
+            <td>".$f['adress']."</td>
+            <td>".$f['product']."</td>
+            <td>".$f['quantity']."</td>
             <td><a href='edit.php?id=".$f['id']."'>edit</a></td>
             <td><a href='delete.php?id=".$f['id']."'>delete</a></td>
             </tr>\n";
